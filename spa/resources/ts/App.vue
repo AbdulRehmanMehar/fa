e7817ccf9a83bc8dacaebb683d654f13982d608e
Abdul Rehman <mehars.6925@gmail.com>
2020-08-08 18:54:07 +0500

    basic vue component

---
+++ spa/resources/ts/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>App</h1>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+
+    export default class App extends Vue{
+        mounted(): void {
+            console.log('component mounteed')
+        }
+    }
+</script>
