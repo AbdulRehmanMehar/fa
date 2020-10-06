7e9ac1c1e16397038e9464917741ae8359111cfa
Abdul Rehman <mehars.6925@gmail.com>
2020-10-07 01:47:45 +0500

    groups page for user profile

---
+++ spa/resources/app/views/authenticated/Profile/Groups.vue
@@ -0,0 +1,221 @@
+<template>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="central-meta">
+					<div class="title-block">
+						<div class="row">
+							<div class="col-lg-6">
+								<div class="align-left">
+									<h5>Groups <span>56</span></h5>
+								</div>
+							</div>
+							<div class="col-lg-6">
+								<div class="row merged20">
+									<div class="col-lg-7 col-md-7 col-sm-7">
+										<form method="post">
+											<input type="text" placeholder="Search Group">
+											<button type="submit"><i class="fa fa-search"></i></button>
+										</form>
+									</div>
+									<div class="col-lg-4 col-md-4 col-sm-4">
+										<div class="select-options">
+											<select class="select">
+												<option>Sort by</option>
+												<option>A to Z</option>
+												<option>See All</option>
+												<option>Newest</option>
+												<option>oldest</option>
+											</select>
+										</div>
+									</div>
+									<div class="col-lg-1 col-md-1 col-sm-1">
+										<div class="option-list">
+											<i class="fa fa-ellipsis-v"></i>
+											<ul>
+												<li><a title="" href="#">Show Friends Public</a></li>
+												<li><a title="" href="#">Show Friends Private</a></li>
+												<li><a title="" href="#">Mute Notifications</a></li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div><!-- title block -->
+				<div class="central-meta padding30">
+					<div class="row">
+						<div class="col-lg-2 col-md-4 col-sm-4">
+							<div class="addgroup">
+								<div class="item-upload">
+									<i class="fa fa-plus-circle"></i>
+									<div class="upload-meta">
+										<h5>Create Group</h5>
+										<span>its only take a few minutes!</span>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-4 col-sm-4">
+							<div class="group-box">
+								<figure><img src="/static/images/resources/group1.jpg" alt=""></figure>
+								<a href="#" title="">Sports Punch</a>
+								<span>125M Members</span>
+								<button>join group</button>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-4 col-sm-4">
+							<div class="group-box">
+								<figure><img src="/static/images/resources/group2.jpg" alt=""></figure>
+								<a href="#" title="">Asian Girls</a>
+								<span>12k Members</span>
+								<button>join group</button>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-4 col-sm-4">
+							<div class="group-box">
+								<figure><img src="/static/images/resources/group3.jpg" alt=""></figure>
+								<a href="#" title="">Graphic Design</a>
+								<span>125M Members</span>
+								<button>join group</button>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-4 col-sm-4">
+							<div class="group-box">
+								<figure><img src="/static/images/resources/group4.jpg" alt=""></figure>
+								<a href="#" title="">Family Lovers</a>
+								<span>1M Members</span>
+								<button>join group</button>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-4 col-sm-4">
+							<div class="group-box">
+								<figure><img src="/static/images/resources/group5.jpg" alt=""></figure>
+								<a href="#" title="">School Mates</a>
+								<span>22M Members</span>
+								<button>join group</button>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-4 col-sm-4">
+							<div class="group-box">
+								<figure><img src="/static/images/resources/group6.jpg" alt=""></figure>
+								<a href="#" title="">Panama Beach</a>
+								<span>5M Members</span>
+								<button>join group</button>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-4 col-sm-4">
+							<div class="group-box">
+								<figure><img src="/static/images/resources/group7.jpg" alt=""></figure>
+								<a href="#" title="">Online Teching</a>
+								<span>52k Members</span>
+								<button>join group</button>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-4 col-sm-4">
+							<div class="group-box">
+								<figure><img src="/static/images/resources/group8.jpg" alt=""></figure>
+								<a href="#" title="">Child Cares</a>
+								<span>1M Members</span>
+								<button>join group</button>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-4 col-sm-4">
+							<div class="group-box">
+								<figure><img src="/static/images/resources/group9.jpg" alt=""></figure>
+								<a href="#" title="">Fun Art</a>
+								<span>35k Members</span>
+								<button>join group</button>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-4 col-sm-4">
+							<div class="group-box">
+								<figure><img src="/static/images/resources/group10.jpg" alt=""></figure>
+								<a href="#" title="">Kids Players</a>
+								<span>10M Members</span>
+								<button>join group</button>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-4 col-sm-4">
+							<div class="group-box">
+								<figure><img src="/static/images/resources/group11.jpg" alt=""></figure>
+								<a href="#" title="">Goldi Friends</a>
+								<span>14M Members</span>
+								<button>join group</button>
+							</div>
+						</div>
+					</div>
+					<div class="lodmore">
+						<span>Viewing 1-11 of 302 Groups</span>
+						<button class="btn-view btn-load-more"></button>
+					</div>
+				</div>
+				<div class="central-meta">
+					<div class="author-info">
+						<h4>Suggested Groups</h4>
+						<p>You May join more these relevant groups.</p>
+					</div>
+					<ul class="related-groups">
+						<li>
+							<div class="group-box">
+								<figure><img src="/static/images/resources/group12.jpg" alt=""></figure>
+								<a href="#" title="">Big Biker</a>
+								<span>15K Members</span>
+								<button>join group</button>
+							</div>
+						</li>
+						<li>
+							<div class="group-box">
+								<figure><img src="/static/images/resources/group13.jpg" alt=""></figure>
+								<a href="#" title="">Blue Tech</a>
+								<span>12k Members</span>
+								<button>join group</button>
+							</div>
+						</li>
+						<li>
+							<div class="group-box">
+								<figure><img src="/static/images/resources/group14.jpg" alt=""></figure>
+								<a href="#" title="">Gold Movies</a>
+								<span>125M Members</span>
+								<button>join group</button>
+							</div>
+						</li>
+						<li>
+							<div class="group-box">
+								<figure><img src="/static/images/resources/group15.jpg" alt=""></figure>
+								<a href="#" title="">Musicly Friends</a>
+								<span>22M Members</span>
+								<button>join group</button>
+							</div>
+						</li>
+						<li>
+							<div class="group-box">
+								<figure><img src="/static/images/resources/group16.jpg" alt=""></figure>
+								<a href="#" title="">AFC Cafe</a>
+								<span>5k Members</span>
+								<button>join group</button>
+							</div>
+						</li>
+						<li>
+							<div class="group-box">
+								<figure><img src="/static/images/resources/group17.jpg" alt=""></figure>
+								<a href="#" title="">Volunteers</a>
+								<span>12k Members</span>
+								<button>join group</button>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+	name: "Groups",
+})
+</script>
