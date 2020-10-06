e9b9178912a14f77591a5d3ca6a664e7144bfb8c
Abdul Rehman <mehars.6925@gmail.com>
2020-10-07 01:44:22 +0500

    the forms

---
+++ spa/resources/app/components/Login.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="we-login-register">
+		<div class="form-title">
+			<i class="fa fa-key"></i>login
+			<span>sign in now and meet the awesome Friends around the world.</span>
+		</div>
+		<form class="we-form" method="post">
+			<input type="text" placeholder="Email">
+			<input type="password" placeholder="Password">
+			<input type="checkbox"><label>remember me</label>
+			<button type="submit" data-ripple="">sign in</button>
+			<a class="forgot underline" href="#" title="">forgot password?</a>
+		</form>
+		<a class="with-smedia facebook" href="#" title="" data-ripple=""><i class="fa fa-facebook"></i></a>
+		<a class="with-smedia twitter" href="#" title="" data-ripple=""><i class="fa fa-twitter"></i></a>
+		<a class="with-smedia instagram" href="#" title="" data-ripple=""><i class="fa fa-instagram"></i></a>
+		<a class="with-smedia google" href="#" title="" data-ripple=""><i class="fa fa-google-plus"></i></a>
+		<span>don't have an account? <router-link class="we-account underline" :to="{ name: 'register' }" title="">register now</router-link></span>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+	name: 'Login',
+
+	mounted() {
+		console.log('mounted login')
+	}
+})
+</script>
+
---
+++ spa/resources/app/components/Register.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="we-login-register">
+		<div class="form-title">
+			<i class="fa fa-key"></i>Sign Up
+			<span>Sign Up now and meet the awesome friends around the world.</span>
+		</div>
+		<form class="we-form" method="post">
+			<input type="text" placeholder="Email">
+			<input type="password" placeholder="Password">
+			<input type="text" placeholder="+1234-567-987">
+			<input type="checkbox"><label>Send code to Mobile</label>
+			<button type="submit" data-ripple="">Register</button>
+			<a class="forgot underline" href="#" title="">forgot password?</a>
+		</form>
+		<a data-ripple="" title="" href="#" class="with-smedia facebook"><i class="fa fa-facebook"></i></a>
+		<a data-ripple="" title="" href="#" class="with-smedia twitter"><i class="fa fa-twitter"></i></a>
+		<a data-ripple="" title="" href="#" class="with-smedia instagram"><i class="fa fa-instagram"></i></a>
+		<a data-ripple="" title="" href="#" class="with-smedia google"><i class="fa fa-google-plus"></i></a>
+		<span>already have an account? <router-link class="we-account underline" :to="{name: 'login'}" title="">Sign in</router-link></span>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+	name: 'Register',
+
+	mounted() {
+		console.log('mounted register')
+	}
+})
+</script>
+
