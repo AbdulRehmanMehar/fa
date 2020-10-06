9637df9dac5b203d6759ccbd24d295fb7facc468
Abdul Rehman <mehars.6925@gmail.com>
2020-10-07 01:45:02 +0500

    vue component for authenticated users

---
+++ spa/resources/app/views/authenticated/Authenticated.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<div class="theme-layout">
+			<div class="postoverlay"></div>
+			<Navbar />
+
+			<!--	For MENU	-->
+			<SidebarLeft />
+
+			<!--	For Messages	-->
+			<SidebarRight />
+
+			<section>
+				<div class="gap2 gray-bg">
+					<router-view></router-view>
+				</div>
+			</section>
+
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Navbar from "@/components/authenticated/Navbar";
+import SidebarLeft from "@/components/authenticated/SidebarLeft";
+import SidebarRight from "@/components/authenticated/SidebarRight";
+
+
+export default Vue.extend({
+	name: "Authenticated",
+
+	components: { Navbar, SidebarLeft, SidebarRight }
+})
+</script>
+
+<style scoped>
+
+</style>
