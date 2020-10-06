e3d423e7e6a551cb696f5cc0e6ff5b397d7c2649
Abdul Rehman <mehars.6925@gmail.com>
2020-10-07 01:40:59 +0500

    django routes to serve vue files

--- spa/views.py
+++ spa/views.py
@@ -1,3 +1,17 @@
+import mimetypes
+import os
+from wsgiref.util import FileWrapper
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, template_name='index.html')
+
+
+def load_js_chunk(request, chunk):
+    content = open(f'spa/public/js/{chunk}', errors='ignore').read()
+    return HttpResponse(content, content_type='application/javascript')
